b03f670d0d7e89b64baf6210b3b2b61bb84fe209
Robin <jonval.robin@gmail.com>
2021-12-09 17:23:51 +0100

    add view an display for list of programs

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Program;
 
 /**
  * @Route("/program", name="program_")
@@ -14,22 +15,36 @@ class ProgramController extends AbstractController
 {
     /**
      * @Route("/", name="index")
+     * @return Response
      */
     public function index(): Response
     {
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
+
         return $this->render('program/index.html.twig', [
-            'website' => 'Wild Séries',
+            'programs' => $programs
         ]);
     }
 
     /**
-     * @Route("/{id}", methods={"GET"}, requirements={"id"="\d+"}, name="show")
+     * @Route("/{id<^[0-9]+$>}", methods={"GET"}, name="show")
+     * @return Response
      */
     public function show(int $id): Response
     {
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program found for id '.$id.' found in program\'s table.'
+            );
+        }
         return $this->render('program/show.html.twig', [
-            'website' => 'Wild Séries',
-            'id' => $id,
+            'program' => $program,
         ]);
     }
 }
