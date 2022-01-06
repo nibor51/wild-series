32e9b2a4ea779a671b89c4c9beafea26beca1fcd
Nibor51 <jonval.robin@gmail.com>
2022-01-06 01:04:56 +0100

    update programtype for init add actor

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -3,10 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Program;
+use App\Entity\Actor;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class ProgramType extends AbstractType
 {
@@ -17,6 +19,12 @@ class ProgramType extends AbstractType
             ->add('summary', TextType::class)
             ->add('poster', TextType::class)
             ->add('category', null, ['choice_label' => 'name'])
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'lastname',
+                'multiple' => true,
+                'expanded' => true,
+            ])
         ;
     }
 
