3fc54d6e6a7e158a8bbc89e5aefb49782aa51355
Nibor51 <jonval.robin@gmail.com>
2022-01-24 12:14:34 +0100

    added security for add category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 use App\Entity\Category;
 use App\Repository\ProgramRepository;
@@ -38,6 +39,7 @@ class CategoryController extends AbstractController
      * The controler for the category add form
      * 
      * @Route("/new", name="new")
+     * @IsGranted("ROLE_ADMIN")
      */
     public function new(Request $request, Slugify $slugify) : Response
     {
